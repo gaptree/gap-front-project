24dbae1d294615410136dced5cb0e6a60992af20
zhanjh <zhanjh@126.com>
2018-07-08 19:02:02 +0800

    zselect

--- front/js/card/zselect.js
+++ front/js/card/zselect.js
@@ -6,7 +6,7 @@ const select = new Zselect({
     required: 'required',
     placeholder: '',
     name: 'userId',
-    isMulti: true,
+    isMulti: false,
     pattern: {
         content: '#{nick} (#{email})',
         selected: '',
@@ -15,5 +15,10 @@ const select = new Zselect({
 });
 
 select.onQuery(query => userRepo.query(query));
+
+select.on('select', item => console.log('select', item));
+select.on('change', items => console.log('change', items));
+
+select.update({selectedItems: [{email: 'z@1.com', nick: 'haha', userId: 'fdsafdas'}]});
 
 select.appendTo(oneElem('.zselect-container'));
